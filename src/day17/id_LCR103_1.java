86389d1ba8f46dfba3eb53d3ca4bb504503684b4
wyt <879590811@qq.com>
2025-03-29 22:21:01 +0800

    不完全背包

---
+++ src/day17/id_LCR103_1.java
@@ -0,0 +1,33 @@
+package day17;
+
+import java.util.Arrays;
+
+public class id_LCR103_1 {
+    private int[] COINS;
+    private int AMOUNT;
+    private int[][] memo;
+
+    public int coinChange(int[] coins, int amount) {
+        this.AMOUNT = amount;
+        this.COINS = coins;
+        int n = coins.length;
+        memo = new int[n][amount + 1];
+        for (int[] row : memo) {
+            Arrays.fill(row, -1);
+        }
+        return dfs(n-1,AMOUNT) < 20000 ? dfs(n-1,AMOUNT) : -1;
+    }
+
+    public int dfs(int i,int c){
+        if(i < 0){
+            return c == 0 ? 0 : 20000;
+        }
+        if(memo[i][c] != -1) return memo[i][c];
+        if(c < COINS[i]){
+            return memo[i][c] = dfs(i - 1,c);
+        }else{
+            return memo[i][c] = Math.min(dfs(i-1,c),dfs(i,c-COINS[i]) + 1);
+        }
+    }
+
+}
---
+++ src/day17/id_LCR103_2.java
@@ -0,0 +1,27 @@
+package day17;
+
+import java.util.Arrays;
+
+public class id_LCR103_2 {
+    private int[][] memo;
+
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        memo = new int[n + 1][amount + 1];
+        for (int[] ints : memo) {
+            Arrays.fill(ints, 20000);
+        }
+        memo[0][0] = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <= amount; j++){
+                if(j < coins[i]){
+                    memo[i+1][j] = memo[i][j];
+                }else {
+                    memo[i+1][j] = Math.min(memo[i][j], memo[i+1][j - coins[i]] + 1);
+                }
+            }
+        }
+        return memo[n][amount] < 20000 ? memo[n][amount] : -1;
+    }
+
+}
